0cf7faf380c93e904e3f51f38340f7b1a328f42a
Carlton Aikins <carltonaikins7@gmail.com>
2022-08-11 22:05:41 -0400

    Format files

--- lib/src/responsive.dart
+++ lib/src/responsive.dart
@@ -5,14 +5,19 @@ class Responsive extends StatelessWidget {
   final Widget tablet;
   final Widget? desktop;
 
-  const Responsive({Key? key, required this.mobile, required this.tablet, this.desktop}) : super(key: key);
+  const Responsive(
+      {Key? key, required this.mobile, required this.tablet, this.desktop})
+      : super(key: key);
 
-  static bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 650;
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 650;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 1100 && MediaQuery.of(context).size.width >= 650;
+      MediaQuery.of(context).size.width < 1100 &&
+      MediaQuery.of(context).size.width >= 650;
 
-  static bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 1100;
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1100;
 
   @override
   Widget build(BuildContext context) {
